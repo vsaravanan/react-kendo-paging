f77acb4f64987e76d10f7e0025bc46155c76f184
vs <saravanan.resume@gmail.com>
2018-06-18 22:56:35 +0800

    fixed axios with paging

--- src/index.js
+++ src/index.js
@@ -2,12 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Grid, GridColumn as Column } from '@progress/kendo-react-grid';
 import axios from 'axios';
-
-//import { products as productimp } from './products.json';
-
 import './extra.css';
-
-//const products = productimp;
 
 
 class App extends React.PureComponent {
@@ -15,27 +10,36 @@ class App extends React.PureComponent {
     state = {
         products : [],
         loading: true,
-        error : ''
+        error : '',
+        pageable: {
+            buttonCount: 5,
+            info: true,
+            type: 'input',
+            pageSizes: true,
+            previousNext: true      
+        },
+        items : [],
+        total:100,
+        skip:0,
+        pageSize:10
 
     }
 
     constructor(props) {
         super(props);
-        this.state = this.createState(0, 10);
         this.pageChange = this.pageChange.bind(this);
-        this.updatePagerState = this.updatePagerState.bind(this);
+
     }
 
     loadData = () => {
         this.setState({ loading: true });
         return axios.get(`http://localhost:2990/products`)
           .then(res => {
-            //const products = res.data;
             this.setState({ 
                 products :  res.data,
                 loading : false
                 });
-            })
+        })
             .catch(error => {
                 console.error(`error: ${error}`);
                 this.setState({
@@ -53,29 +57,16 @@ class App extends React.PureComponent {
     }    
 
     pageChange(event) {
-        this.setState(this.createState(event.page.skip, event.page.take));
+        this.setState( Object.assign({}, this.state, this.createState(event.page.skip, event.page.take))); 
     }
 
     createState(skip, take) {
         return {
-            items: this.state.products.slice(skip, skip + take),
-            total: this.state.products.length,
             skip: skip,
             pageSize: take,
-            pageable: this.state ? this.state.pageable : {
-                buttonCount: 5,
-                info: true,
-                type: 'input',
-                pageSizes: true,
-                previousNext: true
-            }
         };
     }
 
-    updatePagerState(key, value) {
-        const newPageableState = Object.assign({}, this.state.pageable, { [key]: value });
-        this.setState(Object.assign({}, this.state, { pageable: newPageableState }));
-    }
 
     render() {
         const { loading, error, products } = this.state;       
@@ -94,12 +85,13 @@ class App extends React.PureComponent {
             <div>
                 <Grid
                     style={{ height: '280px' }}
-                    data={products}
+                    data={products.slice(this.state.skip, this.state.skip + this.state.pageSize)}
                     onPageChange={this.pageChange}
-                    total={this.state.total}
+                    total={products.length}
                     skip={this.state.skip}
                     pageable={this.state.pageable}
                     pageSize={this.state.pageSize}
+                    
                 >
                     <Column field="ProductID" />
                     <Column field="ProductName" title="Product Name" />
